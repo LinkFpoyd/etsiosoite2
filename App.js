e5fd530f3da57a62443c07cb9fbde6cb963bf726
unknown <leeviva@gmail.com>
2022-03-01 16:31:17 +0200

    nyt toimii

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import * as Location from 'expo-location';
 
 export default function App() {
 
-  const [search, setSearch] = useState('');
-  const [marker, setMarker] = useState({});
-  const [coordinates, setCoordinates] = useState({latitude: 60.200692,
+  const start = {latitude: 60.200692,
     longitude: 24.934302, latitudeDelta: 0.0322,
-    longitudeDelta: 0.0221});
+    longitudeDelta: 0.0221};
+
+  const [search, setSearch] = useState('');
+  const [marker, setMarker] = useState(start);
+  const [coordinates, setCoordinates] = useState(start);
   const [location, setLocation] = useState(null);
 
     useEffect(() => (async () => {
@@ -27,22 +29,22 @@ export default function App() {
           longitudeDelta: 0.0221 })
       })(), []);
 
-  const searchLocation = () => {
-    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=ze9mXWJ57Byb8Vlq7dbzo3a7SxGhVm6L&location=${search}`)
-    .then(response => response.json())
-    .then(data => setMarker(data.results))
-    .catch (error => {
-      Alert.alert('Error', error.toString());
-    });
-
-    console.log(marker);
-
-    let coordinate = {latitude: marker[0].locations[0].latLng.lat,
-      longitude: marker[0].locations[0].latLng.lng,
-      latitudeDelta: 0.0322, longitudeDelta: 0.0221}
-
-    setCoordinates(coordinate)
-  }
+      const searchLocation = async () => {
+        try{
+          const response = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=ze9mXWJ57Byb8Vlq7dbzo3a7SxGhVm6L&location=${search}`);
+          const data = await response.json();
+    
+          let coordinate = {latitude: data.results[0].locations[0].latLng.lat,
+            longitude: data.results[0].locations[0].latLng.lng,
+            latitudeDelta: 0.0322, longitudeDelta: 0.0221}
+      
+          setCoordinates(coordinate)
+    
+    
+        } catch (error) {
+          console.error(error)
+        }
+      }
 
   return (
     <View style={styles.container}>
